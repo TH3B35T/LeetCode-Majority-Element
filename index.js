78bc6f846ac7336f03181073f2fad066d7b2b3f3
Yusuf Mehmet <107208399+TH3B35T@users.noreply.github.com>
2024-02-13 16:10:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,18 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    nums.sort((a,b) => a-b)
-    return nums[Math.floor(nums.length / 2)]
+    // nums.sort((a,b) => a-b)
+    // return nums[Math.floor(nums.length / 2)]
+
+    // Using Boyer-Moore Voting Algorithm. This algorithm has a linear time complexity of O(n) and constant space complexity of O(1),
+    let count = 0;
+    let candidate = null;
+
+    for(let num of nums) {
+        if(count === 0){
+            candidate = num;
+        }
+        count += (candidate === num)? 1:-1;
+    }
+    return candidate;
 };
